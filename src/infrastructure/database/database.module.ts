327447ff33dd77e2158a8e7c11a606194a054a56
anosike prosper <anosikeprosper2@gmail.com>
2025-10-27 22:20:01 +0100

    database setup

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,25 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from '../../application/users/entities/user.entity';
 import { Organization } from '../../application/organizations/entities/organization.entity';
 import { OrganizationMember } from '../../application/organizations/entities/organization-member.entity';
 import { OrganizationInvitation } from '../../application/organizations/entities/organization-invitation.entity';
+import { Ticket } from '../../application/support/entities/ticket.entity';
 import { join } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: Number(process.env.DATABASE_PORT ?? 5433),
-      username: process.env.DATABASE_USER || 'pg',
-      password: process.env.DATABASE_PASSWORD || 'insurlink_password',
-      database: process.env.DATABASE_NAME || 'insurlink',
-      entities: [User, Organization, OrganizationMember, OrganizationInvitation],
-      migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
-      synchronize: process.env.NODE_ENV !== 'production',
-      migrationsRun: process.env.NODE_ENV === 'production',
-      logging: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: Number(configService.get<string>('DATABASE_PORT')),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User, Organization, OrganizationMember, OrganizationInvitation, Ticket],
+        migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        migrationsRun: configService.get<string>('NODE_ENV') === 'production',
+        logging: true,
+      }),
     }),
   ],
 })
